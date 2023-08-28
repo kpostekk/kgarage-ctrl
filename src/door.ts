46afd19b7e784e10b99f218a6c0b9f7d098f22ae
Krystian Postek <krystian@postek.eu>
2023-08-28 16:24:05 +0200

    fix: change handling of stopped states

--- src/door.ts
+++ src/door.ts
@@ -4,100 +4,70 @@ import { CurrentStates, TargetStates } from "./lib/states"
 export class GarageDoorControl extends EventEmitter {
   private target: TargetStates = TargetStates.CLOSE
   private current: CurrentStates = CurrentStates.CLOSE
-  private controlling = false
 
   private readonly changeTimeout = 20_000
 
   constructor(private readonly dryRun?: boolean) {
     super()
-    
+
     if (!this.dryRun) {
       setInterval(() => {
-        if (this.controlling) return
         this.readHardwareState()
         this.emit("current", this.current)
-      }, 5_000)
+      }, 1_000)
     }
   }
 
   public requestTarget(target: TargetStates) {
-    if (
-      this.current !== CurrentStates.CLOSE &&
-      this.current !== CurrentStates.OPEN &&
-      this.current !== CurrentStates.STOPPED
-    ) {
-      this.setCurrent(CurrentStates.STOPPED)
+    this.readHardwareState()
+
+    // if target has changed, but gate has stopped
+    if (this.isStopped) {
+      this.target = Number(!target) as TargetStates
+      this.updateTransitionalState(this.target)
+      this.writeSignal()
       return
     }
 
+    // if target was requested while moving, set stopped state
+    if (this.isMoving) {
+      this.setCurrent(CurrentStates.STOPPED)
+      this.writeSignal()
+      return
+    }
+
+    // ignore target change because it is already set
     if (this.target === target) {
       this.emit("current", this.current)
       return
     }
 
-    this.controlling = true
-
     this.target = target
     this.emit("target", target)
 
-    this.setCurrent(
-      {
-        [TargetStates.OPEN]: CurrentStates.OPENING,
-        [TargetStates.CLOSE]: CurrentStates.CLOSING,
-      }[target],
-    )
-
+    this.updateTransitionalState(target)
     this.writeSignal()
 
-    this.waitForTarget().then(() => {
-      this.controlling = false
-    }).catch(() => {
+    // if gate hasn't reached target state in 20 seconds, set stopped state
+    setTimeout(() => {
+      if (this.targetResolved) return
       this.setCurrent(CurrentStates.STOPPED)
-      this.controlling = false
-    })
+    }, this.changeTimeout)
   }
 
-  private waitForTarget(): Promise<void> {
-    if (this.dryRun) {
-      setTimeout(() => {
-        this.setCurrent(this.target)
-      }, 3000 + Math.random() * 500)
-    }
-
-    return new Promise<void>((resolve, reject) => {
-      const interval = setInterval(() => {
-        this.readHardwareState()
-
-        if (this.current === CurrentStates.STOPPED) {
-          reject()
-          return
-        }
-
-        if (this.targetResolved) {
-          clearInterval(interval)
-          resolve()
-          return
-        }
-      }, 100)
-
-      setTimeout(() => {
-        reject()
-      }, this.changeTimeout)
-    })
-  }
-
+  /**
+   * Reads the hardware state and sets the current state.
+   * This is called every second.
+   * Can be read on demand. 
+   */
   private readHardwareState() {
     if (this.dryRun) return
     const { Gpio } = require("pigpio")
     const p5 = new Gpio(5, { mode: Gpio.INPUT, pullUpDown: Gpio.PUD_UP }) // open state
     const p6 = new Gpio(6, { mode: Gpio.INPUT, pullUpDown: Gpio.PUD_UP }) // close state
 
-    const isGarageOpen = p5.digitalRead() === 1
-    const isGarageClosed = p6.digitalRead() === 1
-
-    if (isGarageOpen && isGarageClosed) {
-      throw new Error("Garage door is in an invalid state")
-    }
+    const isGarageOpen: boolean = p5.digitalRead() && !p6.digitalRead()
+    const isGarageClosed: boolean = p6.digitalRead() && !p5.digitalRead()
 
     if (isGarageOpen) {
       this.setCurrent(CurrentStates.OPEN)
@@ -106,8 +76,18 @@ export class GarageDoorControl extends EventEmitter {
     }
   }
 
+  /**
+   * Writes a HIGH State for 500ms to GPIO 18.
+   * This starts and stops the motor.
+   */
   private writeSignal() {
-    if (this.dryRun) return
+    if (this.dryRun) {
+      setTimeout(() => {
+        this.setCurrent(this.target)
+      }, 5000 + Math.random() * 1000)
+      return
+    }
+
     const { Gpio } = require("pigpio")
     const p4 = new Gpio(18, { mode: Gpio.OUTPUT })
     p4.digitalWrite(1)
@@ -117,6 +97,11 @@ export class GarageDoorControl extends EventEmitter {
   }
 
   private setCurrent(current: CurrentStates) {
+    if (this.current === current) return
+    console.log(new Date(), {
+      from: this.current,
+      to: current,
+    })
     this.current = current
     this.emit("current", this.current)
   }
@@ -124,6 +109,33 @@ export class GarageDoorControl extends EventEmitter {
   public getState() {
     this.readHardwareState()
     return this.current
+  }
+
+  private get isMoving(): boolean {
+    return (
+      this.current === CurrentStates.OPENING ||
+      this.current === CurrentStates.CLOSING
+    )
+  }
+
+  private get isStopped(): boolean {
+    return this.current === CurrentStates.STOPPED
+  }
+
+  private get isSafe(): boolean {
+    return (
+      this.current === CurrentStates.OPEN ||
+      this.current === CurrentStates.CLOSE
+    )
+  }
+
+  private updateTransitionalState(target: TargetStates) {
+    this.setCurrent(
+      {
+        [TargetStates.OPEN]: CurrentStates.OPENING,
+        [TargetStates.CLOSE]: CurrentStates.CLOSING,
+      }[target],
+    )
   }
 
   public get targetResolved(): boolean {
